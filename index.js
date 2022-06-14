a0ee11baad98fd878f51e34a9e0461b5fbcca45c
Snaida.Kabalika <snaida.kabalika@student.moringaschool.com>
2022-06-14 08:44:29 +0300

    commit tree e711e97a9bc1bcb18e82b43644b37bcb6ad1619e

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-function addingEventListener() {
-    const input = document.getElementById('input');
-    
-function clickAlert() {
-  alert('I was clicked!');
-}
-    
-input.addEventListener('click', clickAlert);
-}
 
